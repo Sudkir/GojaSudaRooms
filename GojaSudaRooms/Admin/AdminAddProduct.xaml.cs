640426157368d276df0c4ac03d63d89588b8ae23
root <root@DESKTOP-HOOJO4I>
2021-09-14 14:30:45 +0300

    Добавьте файлы проекта.

---
+++ GojaSudaRooms/Admin/AdminAddProduct.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GojaSudaRooms.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminAddProduct.xaml
+    /// </summary>
+    public partial class AdminAddProduct : Window
+    {
+        public AdminAddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void txbName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbFiltrationCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txbPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txbDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txbMaterial_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnCloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GojaSudaRooms/Auth.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GojaSudaRooms.Helper;
+
+namespace GojaSudaRooms
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public static RoomInteriorsEntities DB = new RoomInteriorsEntities();
+        public Auth()
+        {
+            InitializeComponent();
+           
+            
+        }
+       
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            var userModel = DB.User.FirstOrDefault();
+
+
+        }
+    }
+}
